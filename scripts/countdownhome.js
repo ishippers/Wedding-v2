69c414365c925eacbb66758796de993df9c10d4c
Edward Shiplee <e.shiplee@me.com>
2014-05-12 22:03:13 +0100

    update monday 9pm

---
+++ scripts/countdownhome.js
@@ -0,0 +1,17 @@
+var myCountdown1 = new Countdown({
+							year	: 2015, 
+							month	: 3, 
+							day		: 20, 
+							hour	: 5,
+							ampm	: "pm",
+							minute	: 0, 
+							second	: 0,
+							width	: 400,
+							height	: 60,
+							style 		: "flip",
+							labels		: 	{
+						font 	: "Lustria",
+						color	: "#FFFFFF",
+						weight	: "normal"						// <- no comma on last item!
+					}				// <- no comma on last item!!
+						});
